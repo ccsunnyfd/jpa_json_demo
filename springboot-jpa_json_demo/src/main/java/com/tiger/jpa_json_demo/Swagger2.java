7641990546d912453dd0f7d43b4988379044b60b
ccsunnyfd <chzhyfd@126.com>
2019-06-13 15:09:14 +0800

    add swagger2 document autogeneration(/swagger-ui.html)

---
+++ springboot-jpa_json_demo/src/main/java/com/tiger/jpa_json_demo/Swagger2.java
@@ -0,0 +1,53 @@
+package com.tiger.jpa_json_demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Process
+ *
+ * @version 1.0
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.tiger.jpa_json_demo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swaggers构建RESTful APIs")
+                .description("一个使用springboot jpa构建基本Restful api的例子")
+                .termsOfServiceUrl("https://localhost:8080")
+                .contact("tiger")
+                .version("1.0")
+                .build();
+    }
+
+}
--- springboot-jpa_json_demo/src/main/java/com/tiger/jpa_json_demo/controller/PersonController.java
+++ springboot-jpa_json_demo/src/main/java/com/tiger/jpa_json_demo/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.tiger.jpa_json_demo.controller;
 
 import com.tiger.jpa_json_demo.dao.PersonDao;
 import com.tiger.jpa_json_demo.model.Person;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.*;
@@ -16,23 +18,43 @@ import java.util.List;
 @EnableJpaAuditing
 @RestController
 @RequestMapping(value = "api/person")
+@Api(value = "person信息的增删改查")
 public class PersonController {
     @Autowired
     private PersonDao personDao;
 
     // 如果使用@RequestBody，那么会自动将从前端传过来的json格式的参数封装为java bean对象，可以解决Postman在Post body中发送json格式的数据接收不到的问题
     // 如果不使用@RequestBody，那么vue-resource方式的前端脚本在跨域请求时需要加上{ emulateJSON: true }，使用了@RequestBody则要注释掉这个JSON模拟，否则浏览器会报415错误
+    /**
+     * 添加指定person
+     * api: localhost:8080/api/person/addPerson
+     * @param person
+     */
     @PostMapping(path = "addPerson")
+    @ApiOperation(value="添加指定person")
     public void addPerson(@RequestBody Person person) {
         personDao.save(person);
     }
 
+    /**
+     * 删除指定person
+     * api: localhost:8080/api/person/deletePerson
+     * @param person
+     */
     @DeleteMapping(path = "deletePerson")
+    @ApiOperation(value="删除指定person")
     public void deletePerson(@RequestBody Person person) {
         personDao.delete(person);
     }
 
+    /**
+     * 根据名字查询person信息
+     * api: localhost:8080/api/person/getPerson?name=xxx
+     * @param name
+     * @return
+     */
     @GetMapping("getPerson")
+    @ApiOperation(value = "根据名字查询person信息")
     public List<Person> getPerson(@RequestParam("name") String name) {
         return personDao.findByName(name);
     }
