911edeb574ee4f5977f07f9058031b47ab3ce929
ccsunnyfd <chzhyfd@126.com>
2019-06-23 20:18:36 +0800

    update BlogTable props

--- vue-jpa_json_demo/src/components/ArticleList.vue
+++ vue-jpa_json_demo/src/components/ArticleList.vue
@@ -2,29 +2,31 @@
   <el-container class="article_list">
     <el-main class="main">
       <el-tabs v-model="activeName" @tab-click="handleClick" type="card">
-        <!-- 全部文章列表 -->
+        <!-- authority) -2: 管理员权限 2：普通用户权限 -->
+        <!-- articleState) 1: 已发表 0：草稿箱 2：回收站 -1：全部状态 -->
+        <!-- 全部文章列表，不可编辑，不可删除 -->
         <el-tab-pane label="全部文章" name="all">
-          <blog_table state="-2" :showEdit="false" :showDelete="false" :activeName="activeName"></blog_table>
+          <blog_table authority="2" articleState="-1" :showEdit="false" :showDelete="false" :activeName="activeName"></blog_table>
         </el-tab-pane>
 
         <!-- 已发表文章列表 -->
         <el-tab-pane label="已发表" name="post">
-          <blog_table state="1" :showEdit="true" :showDelete="true" :activeName="activeName"></blog_table>
+          <blog_table authority="2" articleState="1" :showEdit="true" :showDelete="true" :activeName="activeName"></blog_table>
         </el-tab-pane>
 
         <!-- 草稿箱列表 -->
         <el-tab-pane label="草稿箱" name="draft">
-          <blog_table state="0" :showEdit="true" :showDelete="true" :activeName="activeName"></blog_table>
+          <blog_table authority="2" articleState="0" :showEdit="true" :showDelete="true" :activeName="activeName"></blog_table>
         </el-tab-pane>
 
-        <!-- 回收站列表 -->
+        <!-- 回收站列表，不可编辑 -->
         <el-tab-pane label="回收站" name="dustbin">
-          <blog_table state="2" :showEdit="false" :showDelete="true" :activeName="activeName"></blog_table>
+          <blog_table authority="2" articleState="2" :showEdit="false" :showDelete="true" :activeName="activeName"></blog_table>
         </el-tab-pane>
 
-        <!-- 博客管理页 -->
+        <!-- 博客管理页，管理员权限 -->
         <el-tab-pane label="博客管理" name="blogmana" v-if="isAdmin">
-          <blog_table state="-2" :showEdit="false" :showDelete="true" :activeName="activeName"></blog_table>
+          <blog_table authority="-2" articleState="-2" :showEdit="false" :showDelete="true" :activeName="activeName"></blog_table>
         </el-tab-pane>
 
         <!-- 博客配置页 -->
@@ -55,7 +57,7 @@ export default {
     });
   },
   methods: {
-    handleClick(tab, event) {
+    handleClick() {
       // window.console.log(tab, event);
     }
   },
